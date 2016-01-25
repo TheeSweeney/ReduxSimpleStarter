d7574258d9e1d8d86d02b70a2a6bdc85bda50a6a
Brian Sweeney <bsweeney930@gmail.com>
2016-01-24 16:22:47 -0800

    done a lot fo work. probably should have been committing earlier so I could neaten up these comments.

---
+++ src/components/searchbar.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';//these curly braces are equivalent to const Component = React.component//it's saying "import react, and pull of the property Component as a variable called Component"
+
+
+// const SearchBar = function(){//this is a functional component because it literlly is afunction. some info goes in, some jsx comes out.
+//   return <input />;
+// };
+//without the syntax on the first line, we'd have to specifiy React.Component below.
+class SearchBar extends Component {//declares a new class with the name SearchBar. then we use ES6 extend to give it al the functionality of a react class
+  //before we use any kind of state inside an opject, we must first initialize the state object
+  //to initialize state, we set the property state to a plain js obj inside the classes constructor method
+
+  //all js classes have a constructor function. it is the first an only func called when a new instance of a class is created
+  //this func is reserved for doing set-up of class, intializing variables, state, etc
+  constructor(props){
+    super(props);//when we define a method that is already defined on the parent class - Component, which has it's own constructor method - we can call that parent's method by using super
+
+    this.state = {term: ''};//when we use state, we initialize state by create a new obj and assinging it to this.state //the obj we pass will also contain properties that we want to record on the state - in this case, we want to record the prop "term" on state (meaning search term). so we want to update term whenever something is typed into the search bar.
+    //the above code is the only time we will change the state by using standard obj notation
+    //everywhere else we will use this.setState({})
+  }
+
+  render() {//every class based react component must have a render method//this is the ES6 syntax for defining methods on a class
+    //it's a function so we still need to return something //whenever writing jsx and using js variable, we wrap them in curly braces.
+    return (
+      <div>
+        <input onChange={event => this.setState({term: event.target.value})} />
+        Value of the input: {this.state.term}
+      </div>
+    )
+  }//onChange is a react event. more events can be found in docs.
+
+
+  //dealing with events in react has two steps
+  //first we declare an event handler - a fucntion that should be run whenever the event occurs
+  //second we pass that handler to the element that we want to moniter for events. this happens here on line 5
+
+  //all browser events trigger by native elements - div input span button - the event handlers are called with an event object. the actual var name is irrelevant but here it is event. It provides information about what is happening when the event occurs.
+  // onInputChange(event) { //general syntax is on/handle, followed by the name of the element that is being watched for an event, and the name of an event. so on input change, do the below code
+  //   this.setState({term: event.target.value})
+  // }
+  //becasue the above function is super simple, we could refactor our render as such:
+  //return <input onChange={event => console.log(event.target.value)}
+  //and then drop the onInputChange function entirely. this neatens up code a lot. 
+  //becasue we only have on input, we do not need to wrap events in parens
+  
+}
+
+//above is a class component. we make a class when we would like some sort of internal record keeping
+//which is to say when we want it to be aware of itself and what has happened to it since it has been rendered.
+//in general, start witha  functional based component, and then refactor to class once you decide to add new functionality.
+
+
+export default SearchBar; //now any file that imports this file will get back our Searchbar class
+
+//state is a plain js object that is used to record and react to user events
+//each class based component that we define, has it's own state object
+//whenever a components state is changed, it is immediately rerendered as are all of it's child elements.
--- src/index.js
+++ src/index.js
@@ -1,15 +1,54 @@
+//react is a js library used to creat html that will rener to a user
+//component are snippets of code that produce html
+//when we write react code, we write multiple components
+//we nest these components together by placing one inside the other to make complex apps relatively simple
+//a component is a collection of js fucniotns that make html
+  //writing js, but will eventually produce html
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/searchbar';
+const apiKey = 'AIzaSyDRCIzZ1f8j7F1cT79mA2tEN-7bGSnr5pQ';
 
-import App from './components/app';
-import reducers from './reducers';
+//create new component, should produce some html
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () => {//the only difference between this syntax and the below, is the "this" binding. But more or less, this syntax is identical to the below.
+  return (
+  <div>
+    <SearchBar />
+  </div>
+  );
+};
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+/*the above syntax is equal to:
+  return <div>
+      <SearchBar />
+    </div>
+
+  I think I prefer the one not commented out because as this app gets more complex, it'd be nicer for everything to look like inlin html
+  If you choose to use the commented out syntax, be sure the first opening tag appears immediately to the right of return or you will get an error.
+*/
+
+//below is what the above ES6 looks like with mostly ES5 syntax.
+// const App = function(){//const is ES6 syntax//does the same thing as defining a variable, except with const we are saying that this variable (app) will not be reassigned later on.
+//   return <div>Hi!</div>//JSX is a way to write js that looks like html
+// }
+//when we create a component, we are creating a class of a component, or type. 
+//We can have many different instances of App. App itself is a class, not an instance
+//App is a factory that creates instances that are then rendered to the DOM
+
+  //take this generated html and put it on the page/in the DOM
+
+//when we write something like <div></div> into jsx, we are referencing a class of DOM elements, in this example, divs
+//this is interpreted by jsx, as a React.createElement('div', null); so it is creating an instance of that class
+//so, to render an instance of a class, we need only refer to it's html
+//since App above is a class definition, to render it onto the page, we need simply...
+
+//ReactDOM.render(<App />)  //this now creates an instance of App, and passes it to ReactDOM.
+//Above, we are telling React to render this element, but we are not telling it where to go, hence the error "Target container is not a DOM element."
+//so, much like angular requires an ng-view,
+//angular requires a target DOM element, in the below example, the element with the class "container"
+ReactDOM.render(<App />, document.querySelector(".container"))//the div with.container is the root container element of anything rendered using react.
+
+
+//App is a class, <App /> is an instance of that class
